ca0b09b1c7c02552f50c0b9bc92562a382a90341
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-10-14 16:10:31 +0200

    aggiunta struttura IF

--- js/main.js
+++ js/main.js
@@ -28,6 +28,9 @@ let finalPriceMessage;
 // * variabile sconti
 let appliedSale;
 
+// * variabile total price
+let totalPrice;
+
 // # ELABORAZIONE
 // * controllo che il numero di chilometri inserito sia valido
 const isUserKmNumValid = !isNaN(userKmNum) && !userKmNum <= 0;
@@ -41,34 +44,48 @@ const isSaleOverValid = passengerAge >= 65;
 
 // * SE i dati inseriti sono validi
 if (isUserKmNumValid && isPassengerAgeValid) {
-  // calcolo prezzo totale
-  let totalPrice = userKmNum * 0.21;
+  // calcolo prezzo biglietti senza variabili
+  totalPrice = userKmNum * 0.21;
 
   // * SE l'età è minore di 18
   if (isSaleUnderValid) {
-    // calcolo sconto
+    // calcolo sconto 20%
     appliedSale = (totalPrice * 20) / 100;
-    // calcolo prezzo biglietti
-    finalPriceMessage = totalPrice - appliedSale;
   }
+
+  // * ALTRIMENTI SE l'età è maggiore uguale a 65
+  else if (isSaleOverValid) {
+    // calcolo sconto 40%
+    appliedSale = (totalPrice * 40) / 100;
+  }
+
+  // * ALTRIMENTI l'età è compresa tra 18 e 65
+  else {
+    appliedSale = 0;
+  }
+
+  // calcolo prezzo biglietti finale
+  finalPriceMessage = totalPrice - appliedSale;
 }
-// * ALTRIMENTI SE i dati inseriti sono validi AND l'età è maggiore uguale a 65
-// calcolo prezzo biglietti con sconto del 40%
-// * ALTRIMENTI SE i dati inseriti sono validi AND l'età è compresa tra 18 e 65
-// calcolo prezzo biglietti senza sconto
 // * ALTRIMENTI
-// messaggio di errore
+else {
+  // messaggio di errore
+  alert("dati inseriti incorrettamente");
+}
 
 // # OUTPUT
 // * prezzo finale con due decimali
 
+// * debug console
 console.log(`
-userKmNum : ${userKmNum}
-passengerAge : ${passengerAge}
-ticketPriceKm : ${ticketPriceKm}
-finalPriceMessage : ${finalPriceMessage}
-saleUnder : ${saleUnder}
-saleOver : ${saleOver}
-isUserKmNumValid : ${isUserKmNumValid}
-isPassengerAgeValid : ${isPassengerAgeValid}
+    ticketPriceKm : ${ticketPriceKm}
+    userKmNum : ${userKmNum}
+    passengerAge : ${passengerAge}
+    isPassengerAgeValid : ${isPassengerAgeValid}
+    isUserKmNumValid : ${isUserKmNumValid}
+    totalPrice : ${totalPrice}
+    isSaleUnderValid : ${isSaleUnderValid}
+    isSaleOverValid : ${isSaleOverValid}
+    appliedSale : ${appliedSale}
+    finalPriceMessage : ${finalPriceMessage}
 `);
